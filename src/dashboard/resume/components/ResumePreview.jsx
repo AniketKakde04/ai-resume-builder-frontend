0bd9c4d31ebafa00e1e7824524d4a11c4f65a21e
unknown <aniketkakde509@gmail.com>
2025-04-20 20:17:34 +0530

    changes done to ResumePreview.jsx

--- src/dashboard/resume/components/ResumePreview.jsx
+++ src/dashboard/resume/components/ResumePreview.jsx
@@ -8,25 +8,52 @@ import SkillsPreview from './preview/SkillsPreview'
 
 function ResumePreview() {
 
-    const {resumeInfo,setResumeInfo}=useContext(ResumeInfoContext)
+    const { resumeInfo, setResumeInfo } = useContext(ResumeInfoContext)
 
-  return (
-    <div className='shadow-lg h-full p-14 border-t-[20px]'
-    style={{
-        borderColor:resumeInfo?.themeColor
-    }}>
-        {/* Personal Detail  */}
-            <PersonalDetailPreview resumeInfo={resumeInfo} />
-        {/* Summery  */}
-            <SummaryPreview resumeInfo={resumeInfo} />
-        {/* Professional Experience  */}
-           {resumeInfo?.experience?.length>0&& <ExperiencePreview resumeInfo={resumeInfo} />}
-        {/* Educational  */}
-        {resumeInfo?.education?.length>0&&   <EducationalPreview resumeInfo={resumeInfo} />}
-        {/* Skilss  */}
-        {resumeInfo?.skills?.length>0&&    <SkillsPreview resumeInfo={resumeInfo}/>}
-    </div>
-  )
+    return (
+        <div
+            className='shadow-lg h-full p-14 border-t-[20px] 
+                       sm:p-10 md:p-12 lg:p-14 
+                       w-full max-w-[900px] mx-auto 
+                       bg-white 
+                       rounded-lg 
+                       overflow-hidden'
+            style={{
+                borderColor: resumeInfo?.themeColor
+            }}
+        >
+            {/* Personal Detail  */}
+            <div className="mb-6">
+                <PersonalDetailPreview resumeInfo={resumeInfo} />
+            </div>
+
+            {/* Summary */}
+            <div className="mb-6">
+                <SummaryPreview resumeInfo={resumeInfo} />
+            </div>
+
+            {/* Professional Experience */}
+            {resumeInfo?.experience?.length > 0 && (
+                <div className="mb-6">
+                    <ExperiencePreview resumeInfo={resumeInfo} />
+                </div>
+            )}
+
+            {/* Educational */}
+            {resumeInfo?.education?.length > 0 && (
+                <div className="mb-6">
+                    <EducationalPreview resumeInfo={resumeInfo} />
+                </div>
+            )}
+
+            {/* Skills */}
+            {resumeInfo?.skills?.length > 0 && (
+                <div className="mb-6">
+                    <SkillsPreview resumeInfo={resumeInfo} />
+                </div>
+            )}
+        </div>
+    )
 }
 
 export default ResumePreview
