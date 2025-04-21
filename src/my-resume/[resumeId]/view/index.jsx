24685ce754195f6cd9d796b2a5b5227a87093bbd
unknown <aniketkakde509@gmail.com>
2025-04-22 00:27:39 +0530

    upadted myresume/view/index.jsx

--- src/my-resume/[resumeId]/view/index.jsx
+++ src/my-resume/[resumeId]/view/index.jsx
@@ -1,51 +1,66 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import Header from "../../../components/custom/Header"
 import { Button } from '@/components/ui/button'
 import { useParams } from 'react-router-dom'
 import ResumePreview from '@/dashboard/resume/components/ResumePreview'
-import { useState } from 'react'    
-import { useEffect } from 'react'
-import { RWebShare } from "react-web-share";
-
+import { RWebShare } from "react-web-share"
 import GlobalApi from '@service/GlobalApi'
 import { ResumeInfoContext } from '../../../context/ResumeInfoContext'
+import ReactToPrint from 'react-to-print'
+
 function ViewResume() {
+  const [resumeInfo, setResumeInfo] = useState()
+  const { resumeId } = useParams()
+  const componentRef = useRef()
 
-    const [resumeInfo,setResumeInfo]=useState();
-    const {resumeId}=useParams();
+  useEffect(() => {
+    GetResumeInfo()
+  }, [])
 
-    useEffect(()=>{
-        GetResumeInfo();
-    },[])
-    const GetResumeInfo=()=>{
-        GlobalApi.GetResumeById(resumeId).then(resp=>{
-            console.log(resp.data.data);
-            setResumeInfo(resp.data.data);
-        })
-    }
-    const HandleDownload=()=>{
-        window.print();
-    }
-  
+  const GetResumeInfo = () => {
+    GlobalApi.GetResumeById(resumeId).then(resp => {
+      setResumeInfo(resp.data.data)
+    })
+  }
+
   return (
-    <ResumeInfoContext.Provider value={{resumeInfo,setResumeInfo}}>
-        <div id="no-print">
-            <Header/>
+    <ResumeInfoContext.Provider value={{ resumeInfo, setResumeInfo }}>
+      <div id="no-print">
+        <Header />
         <div className='my-10 mx-10 md:mx-20 lg:ms-36'>
-            <h2 className='text-center text-2xl font-medium'>Congratulations! Your AI generated resume is ready</h2>
-            <p className='text-center text-gray-400'>Now you are ready to download resume and you can share unique url with your friends</p>
-            <div className='flex justify-center px-44 my-10'>
-                <Button onClick={HandleDownload} className="theme-button">Download</Button>
+          <h2 className='text-center text-2xl font-medium'>
+            Congratulations! Your AI generated resume is ready
+          </h2>
+          <p className='text-center text-gray-400'>
+            Now you are ready to download resume and you can share unique url with your friends
+          </p>
 
-            </div>
-            </div>
-          
+          {/* React-to-Print Button */}
+          <div className='flex justify-center px-44 my-10'>
+            <ReactToPrint
+              trigger={() => <Button className="theme-button">Download</Button>}
+              content={() => componentRef.current}
+              documentTitle="My_Resume"
+              pageStyle={`
+                @page { margin: 20mm }
+                @media print {
+                  body {
+                    -webkit-print-color-adjust: exact;
+                    print-color-adjust: exact;
+                  }
+                }
+              `}
+            />
+          </div>
         </div>
-        <div>
-        <div id="print-area" className='my-10 mx-10 md:mx-20 lg:ms-36'>
-                <ResumePreview/>
-            </div>
-            </div>
+      </div>
+
+      {/* Resume content to print */}
+      <div ref={componentRef}>
+        <div className='my-10 mx-10 md:mx-20 lg:ms-36'>
+          <ResumePreview />
+        </div>
+      </div>
     </ResumeInfoContext.Provider>
   )
 }
